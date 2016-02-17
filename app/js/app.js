1a85664e61f08d8750b7d85e9b92f3985244ccf1
Mahesh Vangala <vangalamaheshh@gmail.com>
2016-02-17 13:28:42 -0500

    added instrumentation info

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,27 @@
-var mbcfApp = angular.module('mbcfApp',[]);
+var mbcfApp = angular.module('mbcfApp',[
+  'ui.router'
+]);
+
+mbcfApp.config( [ '$stateProvider', '$urlRouterProvider', '$locationProvider',
+        function( $stateProvider, $urlRouterProvider, $locationProvider ) {
+
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+
+    $urlRouterProvider.otherwise('/');
+    
+    $stateProvider
+        .state('/home', {
+            url: '/',
+            templateUrl: 'app/partials/home.html'
+        })
+        .state('/instrumentation', {
+            url: '/instrumentation',
+            templateUrl: 'app/partials/instrumentation.html'
+        });
+
+} ] );
+
+
